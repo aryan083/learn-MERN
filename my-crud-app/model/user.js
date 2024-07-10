091a201b7dddbb92875a4f862cb2dbbf0ba6f736
aryan083 <aryanmahida2@gmail.com>
2024-07-10 12:41:39 +0530

    chore: Update server listen function to include console log

---
+++ my-crud-app/model/user.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const dbConn = require('./config/dbConn');
+const express = require('express');
+
+//get the data from user collection from mongodb this the get request
--- my-crud-app/routes/users.js
+++ my-crud-app/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+ router.post("/" ,function(req,res,next){
+  //add a new user
+  
+ }
+ )
+
 module.exports = router;
